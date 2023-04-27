ca3111b3973910aad5abfaf2d51ce914bea7ae67
yahyabakkar <yahyabakkar022@gmail.com>
2023-04-27 12:53:24 +0300

    validation-email

--- email-validation/email-validation.js
+++ email-validation/email-validation.js
@@ -1,16 +1,33 @@
-function ValidateEmail(inputText)
-{
-var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-if(inputText.value.match(mailformat))
-{
-alert("Valid email address!");
-document.form1.text1.focus();
-return true;
-}
-else
-{
-alert("You have entered an invalid email address!");
-document.form1.text1.focus();
-return false;
-}
-}
+const IsValidEmail = (email) => {
+    //Check minimum valid length of an Email.
+    if (email.length <= 2 || email.length > 256) {
+        return console.log(false);
+    }
+    //Check if email has @ character.
+    if (email.indexOf("@") == -1) {
+        return console.log(false);
+    }
+
+    var splitEmail = email.split("@");
+    var dot = splitEmail[1].indexOf(".");
+    var len = splitEmail[1].length;
+    var splitDots = splitEmail[1].split(".");
+    var countDots = splitDots.length - 1;
+
+
+    //check if dot is available and after the @ character
+    if (dot == -1 || dot < 2 || countDots > 2) {
+        return console.log(false);
+    }
+
+    //Check if Dot is not the last character and dots are not repeated.
+    for (var i = 0; i < splitDots.length; i++) {
+        if (splitDots[i].length == 0) {
+            return console.log(false);
+        }
+    }
+
+    return console.log(true);
+};
+
+IsValidEmail("yahyabakkar022@gmail.com")
